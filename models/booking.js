cc18ca063e0832c1b352b776f510c9127c72d42b
breakfireaus <myoungerc@hotmail.com>
2022-11-17 23:39:52 +1100

    added  booking.js code

--- models/booking.js
+++ models/booking.js
@@ -1,5 +1,56 @@
-// Id (auto)
-// service (fk) 
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class booking extends Model {}
+
+booking.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    bookingtime: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    booking_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    client_id: {
+      type: DataTypes.INTEGER,
+      //Foreign Key
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+
+    service_id: {
+        type: DataTypes.INTEGER,
+        //Foreign Key
+        references: {
+          model: 'service',
+          key: 'id',
+        },
+      },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = booking;
+// service (fk)
 // client id (fk user) (from session, booking time and length)
 // booking time (booking page)
 // booking length (booking page)
