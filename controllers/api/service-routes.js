b61e06455c778670799bdfd1560e456d5bb90daf
James Prince <james.prince1@gmail.com>
2022-11-19 15:51:54 +1100

    Created add, update and delete route definitions. No content

---
+++ controllers/api/service-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const { Service, User } = require("../../models");
+
+
+router.post("/create", withAuth, async (req, res) => {
+
+});
+
+router.put("/update/:id", withAuth, async (req, res) => {
+
+
+});
+
+router.delete("/delete/:id", withAuth, async (req, res) => {
+
+});
