298bd2cd5e83cff6f416d7b7b75839b23fe8832d
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-11-20 14:47:39 +1100

    Added Signin and Signout scripts

---
+++ public/js/signin.js
@@ -0,0 +1,28 @@
+const signinFormHandler = async (event) => {
+    event.preventDefault ();
+
+    const email = document.querySelector('#email-signin').value.trim();
+    const password = document.querySelector('#password-signin').value.trim();
+
+    if (email && password) {
+        // add location of main dashboard
+        const response = await fetch ('/', {
+            method: 'POST',
+            body: JSON.stringify ({email, password}),
+            headers: {
+                'Content-type' : 'application/json'
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert ('Incorrect email or password. Please try again');
+        }
+
+    }
+};
+
+document
+.querySelector('.signin-form')
+.addEventListener ('submit', signinFormHandler);
