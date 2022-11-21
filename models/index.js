09153a64c401737335ce09f8ff274730e1bc5e43
breakfireaus <myoungerc@hotmail.com>
2022-11-21 22:30:21 +1100

    updated index.js with relationships

--- models/index.js
+++ models/index.js
@@ -13,11 +13,36 @@ Service.belongsTo(User, {
   foreignKey: 'id',
 });
 
-//todo: junction table for many to many relationship sql how to do it on sequelize, industry users table
-//todo: one user many bookings
+//one user has many bookings
+User.hasMany(Booking, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+});
+
+Booking.belongsTo(User, {
+  foreignKey: 'id',
+});
+
 //todo: one booking to one user
+Booking.hasOne(User, {
+  foreignKey: 'id'
+});
+
+User.belongsTo(Booking, {
+  foreignKey: 'id',
+});
+
 //todo: one booking to one service
+Booking.hasOne(Service, {
+  foreignKey: 'id',
+});
+
+Service.belongsTo(Booking, {
+  foreignKey: 'id',
+});
 //todo: one service can have many booking
 //todo: many users to many industry
+
+//todo: junction table for many to many relationship sql how to do it on sequelize, industry users table
 
 module.exports = { User, UserIndustry , Booking, Service, Industry };
