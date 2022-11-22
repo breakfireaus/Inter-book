0397c7f9c88b74fa96da1313efba865018220aa0
James Prince <james.prince1@gmail.com>
2022-11-23 10:53:01 +1100

    Added render calls to the service route. I have attempted to pass back meaningful information on the rendering tasks

--- controllers/api/service-routes.js
+++ controllers/api/service-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Service, User } = require("../../models");
+const { Service, User, Booking } = require("../../models");
 
 
 router.post("/create", withAuth, async (req, res) => {
@@ -11,8 +11,10 @@ router.post("/create", withAuth, async (req, res) => {
             !req.body.hourlyRate ||
             !req.body.description
         ) {
-            res.status(400).json({
-                message: "Please include all relevant fields in the body: title, availability, industry, hourlyRay, description"
+            res.render("error", {
+                status: 400,
+                message: "Please include a title, availability, industry, hourly rate and description",
+                logged_in: req.session.logged_in,
             });
             return;
         }
@@ -24,19 +26,22 @@ router.post("/create", withAuth, async (req, res) => {
         const newService = await Service.create({ ...validationObject });
 
         if (!newService) {
-            res.status(500).json({
-                message: "An error occurred when creating a new service",
+            //Render the error page and pass the message back
+            res.render("error", {
+                status: 500,
+                message: "Failed to create service",
+                logged_in: req.session.logged_in,
             });
         } else {
-            res.status(201).json({
-                message: "Service successfully created",
-            });
+            res.render("service", { id: newService.id });
         }
 
     } catch (err) {
         console.error(err);
-        res.status(500).json({
-            message: "An internal server error occurred",
+        res.render("error", {
+            status: 500,
+            message: "An internal server error occurred ",
+            logged_in: req.session.logged_in,
         });
     }
 
@@ -47,16 +52,28 @@ router.put("/update/:id", withAuth, async (req, res) => {
         const serviceToUpdate = await Service.findByPk(req.params.id);
 
         if (!serviceToUpdate) {
-            res.status(404).json({
-                message: "A service with this ID does not exist",
+
+            res.render("error", {
+                status: 404,
+                message: `A service with ID ${req.params.id} does not exist`,
+                logged_in: req.session.logged_in,
             });
+
             return;
         }
 
         if (serviceToUpdate.user_id != req.session.user_id) {
-            res.status(401).json({
-                message: "You are not authorised to modify this service post as it is not yours",
+            res.render("error", {
+                status: 401,
+                message: `You are not authorised to modify service with ID ${req.params.id} as you are not the owner`,
+                logged_in: req.session.logged_in,
             });
+
+            //May want to actually recieve JSON back as it can then be displayed on a text box on the calling page
+            // To discuss
+            // res.status(401).json({
+            //     message: "You are not authorised to modify this service post as it is not yours",
+            // });
             return;
         }
 
@@ -67,9 +84,16 @@ router.put("/update/:id", withAuth, async (req, res) => {
             !req.body.hourlyRate &&
             !req.body.description
         ) {
-            res.status(400).json({
+            res.render("error", {
+                status: 400,
                 message: "Please include at least one updatable field in the request body of title, availability, industry, hourlyRate or description",
+                logged_in: req.session.logged_in,
             });
+            //Similar to above, we may want to actually respond with JSON 
+            //To discuss
+            // res.status(400).json({
+            //     message: "Please include at least one updatable field in the request body of title, availability, industry, hourlyRate or description",
+            // });
             return;
         }
 
@@ -83,13 +107,22 @@ router.put("/update/:id", withAuth, async (req, res) => {
             },
         });
 
-        res.status(200).json({
-            message: "Successfully updated service",
+        const serviceData = await Service.findByPk(req.params.id, {
+            include: [{ model: Booking }]
+        });
+
+        const updatedService = serviceData.get({ plain: true });
+
+        res.render("service", {
+            updatedService,
+            logged_in: req.session.logged_in,
         });
     } catch (err) {
         console.error(err);
-        res.status(500).json({
+        res.render("error", {
+            status: 500,
             message: "An internal server error occurred",
+            logged_in: req.status.logged_in,
         });
     }
 
@@ -105,16 +138,18 @@ router.delete("/delete/:id", withAuth, async (req, res) => {
     //To discuss
     try {
         const serviceToDelete = Service.findByPk(req.params.id);
-        if(!serviceToDelete){
+        if (!serviceToDelete) {
             res.status(404).json({
                 message: "No service with that ID exists",
             });
             return;
         }
 
-        await Service.destroy({where: {
-            id: req.params.id,
-        }});
+        await Service.destroy({
+            where: {
+                id: req.params.id,
+            }
+        });
 
         res.status(200).json({
             message: "Service successfully deleted",
@@ -122,9 +157,7 @@ router.delete("/delete/:id", withAuth, async (req, res) => {
 
     } catch (err) {
         console.error(err);
-        res.status(500).json({
-            message: "An internal server error occurred",
-        });
+        res.render("error", { message: "An internal server error occurred " });
     }
 });
 
