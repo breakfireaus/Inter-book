0fce59158c28f9776bbe87197c9f55dc8316252c
breakfireaus <myoungerc@hotmail.com>
2022-11-24 23:33:06 +1100

    updated page routes

--- controllers/page-routes.js
+++ controllers/page-routes.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
 const withAuth = require("../utils/auth");
 
+router.get ('/', withAuth, async(req,res )=>
+{
+    //todo: render homepage if not signed in login page
+}
+) 
 
+//profile
 
-
+//register
 
 
 module.exports = router;
