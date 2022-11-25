38caf2b4b124eab442a98baeb417fb2d8e60313b
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-11-25 14:13:03 +1100

    Added booking script

--- public/js/booking.js
+++ public/js/booking.js
@@ -1,3 +1,33 @@
-// Add logic for how bookings are created
+const { response } = require("express");
 
-// Add CANCEL booking function
+const addServiceFormHandler = async function (event) {
+    event.preventDefault();
+    
+    const serviceTitle = document.querySelector('#service-title').value;
+    const sessionTime = document.querySelector ('#session-time').value;
+    const description = document.querySelector ('#service-description').value;
+    const paymentMethod = document.querySelector ('#payment-methods').value;
+
+    const response = await fetch ('/bookings', {
+        method: 'POST',
+        body: JSON.stringify ({
+            serviceTitle,
+            sessionTime,
+            description,
+            paymentMethod
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    }
+    else {
+        alert (response.statusText);
+    }
+}
+
+// TO DO: Check add service form ID
+document.querySelector('#service-form').addEventListener('submit', addServiceFormHandler);
