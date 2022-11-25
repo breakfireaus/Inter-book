ab2883f40e76aa4b8001c49e8d4f8c812c60d713
James Prince <james.prince1@gmail.com>
2022-11-25 19:19:41 +1100

    Added basic page routes to display the signin and register views

--- controllers/page-routes.js
+++ controllers/page-routes.js
@@ -2,8 +2,17 @@ const router = require("express").Router();
 const withAuth = require("../utils/auth");
 
 
+router.get("/", withAuth, (req, res)=>{
+    res.render("dashboard");
+});
 
+router.get("/signin", (req, res) => {
+    res.render("signin");
+});
 
+router.get("/register", (req, res) => {
+    res.render("register");
+})
 
 
 module.exports = router;
