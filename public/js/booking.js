66ad08634bbf6d8c922094d2e485aa938b0b0b39
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-11-26 12:26:57 +1100

    Merge branch 'main' of https://github.com/breakfireaus/Inter-book into public/booking.js

--- public/js/booking.js
+++ public/js/booking.js
@@ -29,5 +29,49 @@ const addServiceFormHandler = async function (event) {
     }
 }
 
+// Edit post
+
+// Check querySelector ID 
+
+document.querySelector("#update").addEventListener("click",event=>{
+    event.preventDefault();
+    const bookingID = document.querySelector("#").value;
+    const editBooking = {
+        title:document.querySelector("#").value,
+        content:document.querySelector("#").value,
+    }
+
+    fetch((`/api/bookings/${bookingID}`),{
+        method:"PUT",
+        body:JSON.stringify(editBooking),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            console.log("Booking edited")
+            location.href="/dashboard"
+        } else {
+            alert("An error occurred. Please try again.")
+        }
+    })
+})
+
+document.querySelector("#cancel").addEventListener("click",event=>{
+    event.preventDefault();
+    const bookingID = document.querySelector("#").value;
+    fetch((`/api/blogs/${bookingID}`),{
+        method:"DELETE",
+    }).then(res=>{
+        if(res.ok){
+            console.log("booking cancelled")
+            location.href="/dashboard"
+        } else {
+            alert("An error occurred. Please try again.")
+        }
+    })
+})
+
+
 // TO DO: Check add service form ID
 document.querySelector('#service-form').addEventListener('submit', addServiceFormHandler);
