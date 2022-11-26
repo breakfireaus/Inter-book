d7fd0c5bfbbc91c4ddf567ee9377e39305c4b66f
breakfireaus <myoungerc@hotmail.com>
2022-11-26 17:40:25 +1100

    wrote the code for page routes....not sure if correct

--- controllers/page-routes.js
+++ controllers/page-routes.js
@@ -1,22 +1,31 @@
-const router = require("express").Router();
-const withAuth = require("../utils/auth");
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
 
-router.get ('/', withAuth, async(req,res )=>
-{
-    //todo: render homepage if not signed in login page
-}
-) 
-
-router.get("/", withAuth, (req, res)=>{
-    res.render("dashboard");
+router.get('/', withAuth, async (req, res) => {
+  try {
+    res.render('dashboard', {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get("/signin", (req, res) => {
-    res.render("signin");
-});
+router.get('/signin', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect("dashboard");
+        return;
+      }
+      res.render("signin");
+    });
 
-router.get("/register", (req, res) => {
-    res.render("register");
-})
+router.get('/register', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect("dashboard");
+        return;
+      }
+      res.render("register");
+    });
+  res.render('register');
 
 module.exports = router;
