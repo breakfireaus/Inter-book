97ee4d811611de17cc83a6cddcafb010952912b3
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-11-28 12:33:48 +1100

    Fixed code with suggested changes

--- controllers/api/booking-routes.js
+++ controllers/api/booking-routes.js
@@ -2,38 +2,6 @@ const router = require("express").Router();
 const { Booking, User, Service } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-// GET all bookings 
-
-router.get ('/', withAuth, async (req,res) => {
-    try {
-        const bookingData = await Booking.findAll({
-            include: [{ model: User}, { model: Service }],
-        });
-
-        res.status(200).json(bookingData);
-    } catch (err) {
-        res.status(500).json({message: 'An internal server error occurred 😿'});
-    }
-});
-
-// GET a single booking
-
-router.get ('/:id', withAuth, async (req,res) => {
-    try {
-        const bookingData = await Booking.findByPk(req.params.id, {
-            include: [{ model: User}, { model:Service }],
-        });
-
-    if(!bookingData) {
-        res.status(404).json({ message: 'No Booking with that id exists 😔'});
-        return;
-    }
-        
-    res.status(200).json(bookingData);
-    } catch (err) {
-        res.status(500).json({message: 'An internal server error occurred 😿'});
-    }
-    });
 
 // CREATE a booking 
 
@@ -55,8 +23,8 @@ router.post('/create', withAuth, async (req,res) => {
         else {
         const bookingCard = await Booking.create({
 
-            id: req.body.id
-            client_id: req.body.client_id,
+            id: req.body.id,
+            client_id: req.session.user_id,
             service_id: req.body.service_id,
             cancelled: req.body.cancelled,
             confirmed: req.body.confirmed
@@ -76,17 +44,21 @@ router.post('/create', withAuth, async (req,res) => {
 
 router.update('/update/:id', withAuth, async (req, res) => {
     try {
+
+        const bookingToUpdate = await Booking.findByPk(req.params.id);
+
+        if (!bookingToUpdate) {
+            res.status(404).json({message: 'No Booking with that id exists 😔'});
+        }
+
         const bookingData = await Booking.update({ cancelled: true},
             {where: {
                 id: req.params.id,
             },
         });
-
-        if(!bookingData) {
-            res.status(404).json({message: 'No Booking with that id exists 😔'});
-        }
-
+    
         res.status(200).json(bookingData);
+
     } catch (err) {
         res.status(500).json({message: 'An internal server error occurred 😿'});
     }
